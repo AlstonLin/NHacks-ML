fbf341b7b63801173a50eb17019fd0bb206ef04d
Alston Lin <alstonlin812@gmail.com>
2016-03-12 21:47:18 -0500

    Routing finished

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 80;
 var bodyParser = require('body-parser');
+var ML = require('./ml.js');
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -16,14 +17,17 @@ app.get('/', function (req, res) {
 app.post('/train', function(req, res){
   var url = req.body.url;
   var result = req.body.result;
-  console.log("TRAIN REQUEST: URL: " + url + ", RESULT: " + result);
-  res.end("");
+  console.log("TRAIN REQUEST - URL: " + url + ", RESULT: " + result);
+  ML.train(url, result);
+  res.end();
 });
 
 app.get('/predict/:url', function(req, res){
   var url = req.params.url;
-  console.log("PREDICT REQUEST: URL: " + url);
-  res.end("");
+  console.log("PREDICT REQUEST - URL: " + JSON.stringify(url));
+  res.json({
+    "result" : ML.predict(url)
+  });
 });
 
 app.listen(PORT, function () {
---
+++ ml.js
@@ -0,0 +1,7 @@
+module.exports = {
+  train : function(url, result){
+  },
+  predict : function(url){
+    return 0.8;
+  }
+};
