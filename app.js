85d1cafc01635a8deac367698416f57b7e82ff10
Ubuntu <ubuntu@ip-172-31-54-225.ec2.internal>
2016-03-13 01:29:33 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = 80;
+app.get('/', function (req, res) {
+  res.end('Hello World!');
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '!');
+});
+
